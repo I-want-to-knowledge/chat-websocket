5fbe7631f06a58adacf7430ca309048705f9fc86
yanzhen <1031881200@qq.com>
2019-01-25 17:18:14 +0800

    代码优化！！

--- src/main/java/com/example/demo/WebSocketServer.java
+++ src/main/java/com/example/demo/WebSocketServer.java
@@ -1,28 +1,22 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Component;
+
+import javax.websocket.*;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.ConcurrentHashMap;
 
-import javax.websocket.EndpointConfig;
-import javax.websocket.OnClose;
-import javax.websocket.OnError;
-import javax.websocket.OnMessage;
-import javax.websocket.OnOpen;
-import javax.websocket.Session;
-import javax.websocket.server.PathParam;
-import javax.websocket.server.ServerEndpoint;
-
-import org.springframework.stereotype.Component;
-
 /**
  * test webSocket
  * 
- * @ServerEndpoint 注解是一个类层次的注解，它的功能主要是将目前的类定义成一个websocket服务器端,
+ * Server Endpoint 注解是一个类层次的注解，它的功能主要是将目前的类定义成一个websocket服务器端,
  *                 注解的值将被用于监听用户连接的终端访问URL地址,客户端可以通过这个URL来连接到WebSocket服务器端
- * @ServerEndpoint 可以把当前类变成websocket服务类
+ * Server Endpoint 可以把当前类变成websocket服务类
  *
  * @author YanZhen 2018-04-23 13:52:49 WebSocketTest
  */
@@ -32,7 +26,7 @@ public class WebSocketServer {
 	// 静态变量，用来记录当前在线连接数。应该把它设计成线程安全的。
 	private static int onlineCount = 0;
 	// concurrent包的线程安全Set，用来存放每个客户端对应的MyWebSocket对象。若要实现服务端与单一客户端通信的话，可以使用Map来存放，其中Key可以为用户标识
-	private static ConcurrentHashMap<String, WebSocketServer> webSocketSet = new ConcurrentHashMap<String, WebSocketServer>();
+	private static ConcurrentHashMap<String, WebSocketServer> webSocketSet = new ConcurrentHashMap<>();
 	// 与某个客户端的连接会话，需要通过它来给客户端发送数据
 	private Session session;
 	// 当前发消息的人员编号
@@ -43,9 +37,10 @@ public class WebSocketServer {
 	 *
 	 * @param session
 	 *          可选的参数。session为与某个客户端的连接会话，需要通过它来给客户端发送数据
+	 *          可选参数 EndpointConfig
 	 */
 	@OnOpen
-	public void onOpen(@PathParam(value = "userno") String param, Session session, EndpointConfig config) {
+	public void onOpen(@PathParam(value = "userno") String param, Session session) {
 		System.out.println(param);
 		if (webSocketSet.get(param) == null) {
 			userno = param;// 接收到发送消息的人员编号
@@ -74,17 +69,16 @@ public class WebSocketServer {
 	 *
 	 * @param message
 	 *          客户端发送过来的消息
-	 * @param session
-	 *          可选的参数
+	 * session 可选的参数
 	 */
 	@OnMessage
-	public String onMessage(String message, Session session) {
+	public String onMessage(String message) {
 		System.out.println("来自客户端的消息:" + message);
 		
 		String sendUserno = message.split("[|]")[1];
 		String sendMessage = message.split("[|]")[0];
 		
-		String msg = null;
+		String msg;
 		
 		// 群发消息
 		if (userno.equals(sendUserno)) {
@@ -99,8 +93,8 @@ public class WebSocketServer {
 	/**
 	 * 给指定的人发送消息
 	 * 
-	 * @param sendMessage
-	 * @param sendUserno
+	 * @param sendMessage 发送的信息
+	 * @param sendUserno 发送用户编号
 	 */
 	private String sendToUser(String sendMessage, String sendUserno) {
 		// String sendUserno = message.split("[|]")[1];
@@ -125,7 +119,7 @@ public class WebSocketServer {
 	/**
 	 * 给所有人发消息
 	 * 
-	 * @param sendMessage
+	 * @param sendMessage 发送的信息
 	 */
 	private String sendAll(String sendMessage) {
 		String now = getNowTime();
@@ -151,23 +145,22 @@ public class WebSocketServer {
 	/**
 	 * 获取当前时间
 	 *
-	 * @return
+	 * @return 当前时间 yyyy-MM-dd HH:mm:ss
 	 */
 	private String getNowTime() {
 		Date date = new Date();
 		DateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		String time = format.format(date);
-		return time;
+		return format.format(date);
 	}
 
 	/**
 	 * 发生错误时调用
 	 *
-	 * @param session
 	 * @param error
+	 * session 可选参数
 	 */
 	@OnError
-	public void onError(Session session, Throwable error) {
+	public void onError(Throwable error) {
 		System.out.println("发生错误");
 		error.printStackTrace();
 	}
@@ -175,23 +168,23 @@ public class WebSocketServer {
 	/**
 	 * 这个方法与上面几个方法不一样。没有用注解，是根据自己需要添加的方法。
 	 *
-	 * @param message
-	 * @throws IOException
+	 * @param message 发送的信息
+	 * @throws IOException 失败或中断I/O操作产生的异常
 	 */
-	public void sendMessage(String message) throws IOException {
+	private void sendMessage(String message) throws IOException {
 		this.session.getBasicRemote().sendText(message);
 		// this.session.getAsyncRemote().sendText(message);
 	}
 
-	public static synchronized int getOnlineCount() {
+	private static synchronized int getOnlineCount() {
 		return onlineCount;
 	}
 
-	public static synchronized void addOnlineCount() {
+	private static synchronized void addOnlineCount() {
 		WebSocketServer.onlineCount++;
 	}
 
-	public static synchronized void subOnlineCount() {
+	private static synchronized void subOnlineCount() {
 		WebSocketServer.onlineCount--;
 	}
 
