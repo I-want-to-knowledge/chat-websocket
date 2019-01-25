a0740517dbc308940a64641a3f6d6edfca5e934b
yanzhen <1031881200@qq.com>
2019-01-25 17:08:58 +0800

    修改web打包需要用war包！
    使用ip为公网ip

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -18,7 +18,8 @@ public class HelloController {
 	@RequestMapping("/index")
 	public String index(Map<String, Object> map, HttpServletRequest request) {
 		map.put("name", "Test WebSocket");
-		map.put("ip", request.getLocalAddr());
+//		map.put("ip", request.getLocalAddr());
+		map.put("ip", "39.96.23.254");// 公网ip
 		map.put("port", request.getServerPort());
 		map.put("path", request.getServletPath());
 		System.out.println(map.toString());
